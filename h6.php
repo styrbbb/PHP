dd527c2ca15440c67297d28dddaefec0de115139
thorian <115464923+styrbbb@users.noreply.github.com>
2024-02-14 09:05:52 +0200

    PHP harjutus 6
    PHP

---
+++ h6.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="et">
+    <head>
+        <title>Harjutus 05</title>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+
+        <!-- Bootstrap CSS v5.2.1 -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous"/>
+    </head>
+    <body>
+        <div class="container">
+        <h2>.</h2>
+        <?php   
+        /*  Harjutus 05
+            Thorian Perk
+            07.02.2024
+        */
+
+        ?>
+        </div>
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+    </body>
+</html>
