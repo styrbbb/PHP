28e3ad5fd5014c1c91aca85f2494117cb7ac86ae
thorian <115464923+styrbbb@users.noreply.github.com>
2024-02-17 17:20:47 +0200

    PHP harjutus 6
    p

--- h6.php
+++ h6.php
@@ -1,50 +1,53 @@
 <!doctype html>
 <html lang="et">
-    <head>
-        <title>Harjutus 06</title>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
 
-        <!-- Bootstrap CSS v5.2.1 -->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous"/>
-    </head>
-    <body>
-        <div class="container">
+<head>
+    <title>Harjutus 06</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+
+    <!-- Bootstrap CSS v5.2.1 -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
+</head>
+
+<body>
+    <div class="container">
         <h2>Genereeri</h2>
-        <?php   
+        <?php
         /*  Harjutus 06
             Thorian Perk
             14.02.2024
         */
         echo "<b>Arvud 1-100</b><br>";
-        for($arv0=1;$arv0<=100;$arv0++){
-            echo $arv0."\n"; 
+        for ($arv0 = 1; $arv0 <= 100; $arv0++) {
+            echo $arv0 . "\n";
         }
         echo "<br>";
         echo "<br>";
         echo "<b>Arvud reavahetuses</b><br>";
-        for ($arv1 = 0; $arv1 <= 100; $arv1+=10) {
-            echo $arv1. '<br>';
+        for ($arv1 = 0; $arv1 <= 100; $arv1 += 10) {
+            echo $arv1 . '<br>';
         }
         echo "<br>";
         echo "<b>Punktid arvu järel</b><br>";
         for ($arv2 = 1; $arv2 <= 100; $arv2++) {
-            echo $arv2. ". \n";
-        } 
+            echo $arv2 . ". \n";
+        }
         ?>
         <h1 class="mb-3"></h1>
         <h2>Read</h2>
         <?php
         echo "<b>Horisontaalne</b><br>";
-        for($rida=1; $rida<=1; $rida++){ 
-            for($veerg=1; $veerg<=10; $veerg++){ 
-                    echo "* \n";	
-                }
+        for ($rida = 1; $rida <= 1; $rida++) {
+            for ($veerg = 1; $veerg <= 10; $veerg++) {
+                echo "* \n";
+            }
             echo '<br>';
         }
         echo "<br>";
         echo "<b>Vertikaalne</b><br>";
-        for($rida=1; $rida<=10; $rida++){ 
+        for ($rida = 1; $rida <= 10; $rida++) {
             echo '*<br>';
         }
         ?>
@@ -56,12 +59,12 @@
             <input type="submit" class="btn btn-success my-2" value="Arvuta"><br>
         </form>
         <?php
-        if(isset($_GET['kylg'])) {
+        if (isset($_GET['kylg'])) {
             $kylg = $_GET['kylg'];
 
-            if(is_numeric($kylg) && $kylg > 0 && $kylg == round($kylg)) {
-                for($i = 0; $i < $kylg; $i++) {
-                    for($j = 0; $j < $kylg; $j++) {
+            if (is_numeric($kylg) && $kylg > 0 && $kylg == round($kylg)) {
+                for ($i = 0; $i < $kylg; $i++) {
+                    for ($j = 0; $j < $kylg; $j++) {
                         echo '* &nbsp;&nbsp;&nbsp;';
                     }
                     echo "<br>";
@@ -73,23 +76,26 @@
         ?>
         <h1 class="mb-3"></h1>
         <h2>Kahanev</h2>
-        <?php 
+        <?php
         echo "<br>";
         $knm = 10;
-        while($knm>0) {
-          echo $knm.'<br>';
-          $knm--;
+        while ($knm > 0) {
+            echo $knm . '<br>';
+            $knm--;
         }
         ?>
         <h1 class="mb-3"></h1>
-        <h2>Kolmega jagunevad</h2>       
-        <?php 
-        
-        
+        <h2>Kolmega jagunevad</h2>
+        <?php
+
+
         ?>
 
-        </div>
+    </div>
 
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
-    </body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
+        integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
+        crossorigin="anonymous"></script>
+</body>
+
 </html>
