b8d5662f7c86198d142c131224485af79a88e642
thorian <115464923+styrbbb@users.noreply.github.com>
2024-03-14 19:56:59 +0200

    PHP harjutus 10

---
+++ h10.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="et">
+    <head>
+        <title>Harjutus 09</title>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+
+        <!-- Bootstrap CSS v5.2.1 -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous"/>
+    </head>
+    <body>
+        <div class="container">
+        <h1>Tekstifunktsioonid</h1>
+        <h1 class="mb-4"></h1>
+        <h2>hj .1</h2>
+        <?php   
+        /*  Harjutus 10
+            Thorian Perk
+            14.03.2024
+        */
+
+        $nimi = 'mARiO';
+	
+        echo ucwords(strtolower('Tere, '. $nimi));
+        ?>
+
+        <h1 class="mb-4"></h1>
+        <h2></h2>
+        </div>
+
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+    </body>
+</html>
