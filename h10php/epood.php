176f3c837d82f0ac3cc7c2e92fda6dc6b0086638
thorian <115464923+styrbbb@users.noreply.github.com>
2024-03-14 20:17:46 +0200

    PHP harjutus 10

---
+++ h10php/epood.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="et">
+    <head>
+        <title>Harjutus 10</title>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+
+        <!-- Bootstrap CSS v5.2.1 -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous"/>
+    </head>
+    <body>
+        <div class="container">
+        <h1>e-pood</h1>
+        <h1 class="mb-4"></h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellendus magnam cupiditate, quidem molestias labore veniam voluptatibus, ea corporis quibusdam quam quia perspiciatis neque delectus? Recusandae alias libero inventore minus porro.</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellendus magnam cupiditate, quidem molestias labore veniam voluptatibus, ea corporis quibusdam quam quia perspiciatis neque delectus? Recusandae alias libero inventore minus porro.</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellendus magnam cupiditate, quidem molestias labore veniam voluptatibus, ea corporis quibusdam quam quia perspiciatis neque delectus? Recusandae alias libero inventore minus porro.</p>
+
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+    </body>
+</html>
