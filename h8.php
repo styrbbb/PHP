5dd416e40b9698bd870629be20ea47b2f87c80da
thorian <115464923+styrbbb@users.noreply.github.com>
2024-04-29 21:30:20 +0300

    PHP 8

--- h8.php
+++ h8.php
@@ -7,12 +7,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 
     <!-- Bootstrap CSS v5.2.1 -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js"
-        integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT"
-        crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 
 <body>
@@ -74,10 +70,11 @@
         <h1 class="mb-4"></h1>
         <h2>Aastaajad</h2>
         <div class="dropdown">
-            <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown"
+            <a class="btn btn-secondary dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
                 aria-expanded="false">
                 Vali aastaaeg
-            </button>
+            </a>
+
             <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
                 <li><a class="dropdown-item" href="?a=suvi">Suvi</a></li>
                 <li><a class="dropdown-item" href="?a=sugis">Sügis</a></li>
@@ -85,11 +82,36 @@
                 <li><a class="dropdown-item" href="?a=kevad">Kevad</a></li>
             </ul>
         </div>
+
+        <?php
+        // PHP logic to display the selected image
+        if (isset($_GET['season'])) {
+            $season = $_GET['season'];
+            switch ($season) {
+                case 'talv':
+                    $image = 'talv.jpg';
+                    break;
+                case 'kevad':
+                    $image = 'kevad.jpg';
+                    break;
+                case 'suvi':
+                    $image = 'suvi.jpg';
+                    break;
+                case 'sugis':
+                    $image = 'sugis.jpg';
+                    break;
+                default:
+                    $image = 'default.jpg'; // Default image if season not recognized
+                    break;
+            }
+            echo '<img src="' . $image . '" alt="' . ucfirst($season) . '" class="img-fluid">';
+        }
+
+        ?>
     </div>
 
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
-        integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
         crossorigin="anonymous"></script>
 </body>
 
