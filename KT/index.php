36be05e23e04ff3a9ec67873ac4f156831c07a18
thorian <115464923+styrbbb@users.noreply.github.com>
2024-05-29 19:24:22 +0300

    KT

--- KT/index.php
+++ KT/index.php
@@ -6,10 +6,9 @@
     <title>PHP KT</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" 
     integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css">
 </head>
 <body>
-  <nav class="navbar navbar-expand-lg bg-dark bg-body-tertiary" data-bs-theme="dark">
+  <nav class="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
     <div class="container">
       <a class="navbar-brand" href="#">thorian.com</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -21,30 +20,28 @@
                 <a class="nav-link active" aria-current="page" href="#">Avaleht</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="tooted.html">Pood</a>
+                <a class="nav-link" href="pood.html">Pood</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="teenused.html">Kontakt</a>
+                <a class="nav-link" href="kontakt.html">Kontakt</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="kontakt.html">Admin</a>
+                <a class="nav-link" href="admin.html">Admin</a>
               </li>
             </ul>
           </div>
         </nav>
         <div class="container">
-          <div class="row p-5 mb-4">
-            <div class="col-sm-3 text-center">
-              <h2 class="text-center">Lorem Ipsum</h2>
-              <?php include'KT_rand1'?>
-              <?php echo random1("KT_rand_img"); ?>
-              <!--<p class="text-center"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis quisquam nihil voluptatibus voluptates expedita nulla maxime odit molestias quaerat molestiae ut ipsum et qui quam excepturi necessitatibus maiores, ducimus ullam.</p> -->
+          <div class="row justify-content-evenly p-5 mb-4">
+              <div class="col-md-6">
+                <?php include 'KT_rand1'; ?>
+                <img class="img-fluid" src="random1" alt="">
+                <?php echo random1("KT_rand_img"); ?>
             </div>
-            <div class="col-sm-3 text-center">
-              <h2 class="text-center">Lorem Ipsum</h2>
-              <?php include'KT_rand2'?>
-              <?php echo random2("KT_rand_img"); ?>
-              <!--<p class="text-center"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis quisquam nihil voluptatibus voluptates expedita nulla maxime odit molestias quaerat molestiae ut ipsum et qui quam excepturi necessitatibus maiores, ducimus ullam.</p> -->
+            <div class="col-md-6">
+                <?php include 'KT_rand2'; ?>
+                <img class="img-fluid" src="random2" alt="">
+                <?php echo random2("KT_rand_img"); ?>
             </div>
           </div>
         </div>
