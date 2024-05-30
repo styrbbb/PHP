8d157a803baac642a92240b6affa2e9ed5bbe6ab
thorian <115464923+styrbbb@users.noreply.github.com>
2024-05-30 20:48:50 +0300

    KT

--- KT/admin.php
+++ KT/admin.php
@@ -8,29 +8,6 @@
     integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
-        <div class="container">
-        <a class="navbar-brand" href="#">thorian.com</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="minuMenyy">
-                <ul class="navbar-nav ms-auto">
-                <li class="nav-item">
-                    <a class="nav-link" aria-current="page" href="#">Avaleht</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="pood.php">Pood</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="kontakt.php">Kontakt</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link  active" href="#">Admin</a>
-                </li>
-                </ul>
-            </div>
-        </nav>
   
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" 
   integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
